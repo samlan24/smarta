5e8bb1015b1a9b213a8ed1863ee29b9c2c56c282
samlan24 <muchemionline@gmail.com>
2025-09-07 18:22:51 +0300

    feat(ui): add welcome message on dashboard

--- smart_commit/src/app/dashboard/page.tsx
+++ smart_commit/src/app/dashboard/page.tsx
@@ -14,6 +14,7 @@ export default async function DashboardPage() {
         Welcome, {user?.user_metadata.full_name || "Google User"} 👋
       </h2>
       <p className="text-gray-600">{user?.email}</p>
+      <p className="text-gray-600">enjoy</p>
       <SignOutButton />
     </div>
   );
